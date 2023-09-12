f0e10dc3307ef9ee9cbf4aa83456162bcf16a50f
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-09-12 13:05:38 +0100

    added file structure and project starter files

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/abdelkrim/repos/e-commerce-app/app/layout.tsx": [
+      "/home/abdelkrim/repos/e-commerce-app/styles/globals.css",
+      "/home/abdelkrim/repos/e-commerce-app/node_modules/next/font/google/target.css?{\"path\":\"lib/fonts.ts\",\"import\":\"Plus_Jakarta_Sans\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-sans\"}],\"variableName\":\"fontSans\"}",
+      "/home/abdelkrim/repos/e-commerce-app/node_modules/next/font/google/target.css?{\"path\":\"lib/fonts.ts\",\"import\":\"JetBrains_Mono\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-mono\"}],\"variableName\":\"fontMono\"}"
+    ]
+  },
+  "cssModules": {
+    "/home/abdelkrim/repos/e-commerce-app/app/page": [
+      "/home/abdelkrim/repos/e-commerce-app/styles/globals.css",
+      "/home/abdelkrim/repos/e-commerce-app/node_modules/next/font/google/target.css?{\"path\":\"lib/fonts.ts\",\"import\":\"Plus_Jakarta_Sans\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-sans\"}],\"variableName\":\"fontSans\"}",
+      "/home/abdelkrim/repos/e-commerce-app/node_modules/next/font/google/target.css?{\"path\":\"lib/fonts.ts\",\"import\":\"JetBrains_Mono\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-mono\"}],\"variableName\":\"fontMono\"}"
+    ]
+  }
+}
