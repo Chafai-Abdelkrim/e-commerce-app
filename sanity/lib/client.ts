92abb9b7ed384ccb86c11ab807208e4c6b497f81
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-09-19 11:51:39 +0100

    removed token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skPP0iTAUq25Ry69V9uFzcvhyBW4Ud0CBoEaXLjQOydxxIB6FghIDADjFTKkaFpQWR9i0P9MNuJjsxjnu65U7dK8YtNxDQ7LEwuAKk9i66hYjyJuZLWVS9i2yIjDaPK3vGaVz6sPqWnE3unhuwx0ICD7sJFV9h5La2lcwS4OCgErNPPpLIZq",
 })
